dedfb1cd00e0bd41eb107c4b09032254d7e1f378
Adrian Staniec <adrianstaniec@gmail.com>
2018-05-07 22:11:48 +0200

    switch to UbuntuMono font

--- config.def.h
+++ config.def.h
@@ -7,12 +7,13 @@
  */
 
 // static char *font = "DejaVuSansMono Nerd Font Mono:pixelsize=17:antialias=true:autohint=true";
-/* static char *font = "Hack Nerd Font Mono:pixelsize=17:antialias=true:autohint=true"; */
+// static char *font = "Hack Nerd Font Mono:pixelsize=17:antialias=true:autohint=true";
 // static char *font = "Hack:pixelsize=17:antialias=true:autohint=true";
 // static char *font = "LiterationMono Nerd Font Mono:pixelsize=17:antialias=true:autohint=true";
-static char *font = "Inconsolata Nerd Font Mono:pixelsize=17:antialias=true:autohint=true";
+// static char *font = "Inconsolata Nerd Font Mono:pixelsize=17:antialias=true:autohint=true";
 // static char *font = "RobotoMono Nerd Font Mono:pixelsize=17:antialias=true:autohint=true";
 // static char *font = "SauceCodePro Nerd Font Mono:pixelsize=17:antialias=true:autohint=true";
+static char *font = "UbuntuMono Nerd Font Mono:pixelsize=17:antialias=true:autohint=true";
 
 static int borderpx = 2;
 
